c1016db29db6407eb2b0aca5298bd864544d42ab
theOehrly <23384863+theOehrly@users.noreply.github.com>
2023-05-15 21:52:13 +0200

    DOC: fix build

--- docs/conf.py
+++ docs/conf.py
@@ -18,12 +18,13 @@ from datetime import datetime
 
 # -- Project information -----------------------------------------------------
 
-# project = 'Timple'
+project = 'Timple'
 copyright = f'{datetime.now().year}, theOehrly'
 author = 'theOehrly'
 
 # The full version, including alpha/beta/rc tags
-# release = '0.1.0'
+version = '0.1.6'
+release = version
 
 
 # -- General configuration ---------------------------------------------------
